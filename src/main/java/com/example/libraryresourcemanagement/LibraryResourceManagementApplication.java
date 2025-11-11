101cb03ce2c3bc681f70d3523129f085a2d398d1
edbless <giftyhemans8@gmail.com>
2025-11-11 11:03:13 +0000

    Project start

---
+++ src/main/java/com/example/libraryresourcemanagement/LibraryResourceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.libraryresourcemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryResourceManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryResourceManagementApplication.class, args);
+    }
+
+}
